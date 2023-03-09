a3bb3066981fbf85fdeacf492c91273a5d361b06
cosmas rono tiongik <ccosmas001@gmail.com>
2023-03-09 14:19:47 +0300

    change

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <template>
-  <main class=" bg-black text-white">
-    <img src="" alt="">
+    <main 
 
+
+class=" top-0 w-full h-full bg-cover text-white" style="
+     background-image: url('https://img.freepik.com/free-vector/worldwide-connection-green-background-illustration-vector_53876-61766.jpg?w=826&t=st=1678356933~exp=1678357533~hmac=7b430b72fd499354ec3d55c3a06346ec64de7a13220384ee01b5265dae7b211f');">
+  
   <div class=" text-xl flex flex-row gap-4 items-center justify-center">
     <nuxt-link to="/">Home</nuxt-link>
     <nuxt-link to="/">About</nuxt-link>
@@ -13,7 +16,8 @@
 
     <NuxtPage />
   
-  </div></main>
+  </div>
+</main>
 </template>
 <script>
  import backgroundUrl from '~/assets/back.jpg'
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,32 @@
 <template>
-    <main class="">
-  <div class=" ">
+ 
+    
+
+
       <h1 class=" flex items-center justify-center text-white"> Hello here is cosmas feel free and relax </h1>
       <div class=" text-black text-xl  md:mt-1 md:left top-1 md:ml-2  md:relative  md:flex ">
         <div class=" relative left-14"> 
      <div class=" flex items-center justify-end pr-6 pl-6 px-3 my-4 mx-60 rounded-md border-slate-700  text-white">
-        <div class=" "><img src="../assets/one.jpg" class="  h-full w-full ">
+     
+
+  <div class="swiper-container">
+    <div class="swiper-wrapper flex flex-row">
+      <div class="swiper-slide ">
+        <img src="../assets/nuxt.png" alt="  1" />
       </div>
+      <div class="swiper-slide">
+        <img src="../assets/django.png" alt="  2" />
+      </div>
+      <div class="swiper-slide">
+        <img src="../assets/tailwindcss.png" alt="  3" />
+      </div>
+      <div class="swiper-slide">
+        <img src="../assets/vuejs.png" alt="  4" />
+      </div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+      
      </div>
         </div>
       </div>
@@ -15,11 +35,11 @@
         <aside class=" md:mt-1 md:w-1/5   md:left top-1 md:ml-2 h-full  md:fixed md:min-h-screen md:flex " aria-label="Sidebar">
     <div class="  overflow-y-auto py-2 rounded  dark:bg-slate-900  pb-40 mb-20 mt-24">
      <div class=" flex justify-center items-center mx-auto flex-col gap-y-2">
-     <img src="../assets/cossi.jpeg" alt="" class=" flex items-center rounded-full h-30 w-40 ring-4 ring-blue-800  dark:ring-white">
+     <img src="../assets/prof.png" alt="" class=" flex items-center rounded-full h-30 w-40 ring-4 ring-blue-800  dark:ring-white">
     <h1 class="  text-blue-900   dark:text-gray-300 font-bold text-4xl  font-mono">Cossi wing</h1>
-    <p class="   dark:text-gray-300 pb-16 flex justify-center items-center">
-                         I am a professional,web developer,Graphic designer, 
-                         Research Writer/ Tutor with five years of experience hence you can rest assured that I will guarantee you quality, 
+    <p class="   dark:text-gray-300 bg-white6 flex justify-center items-center">
+                         I am a software developer,Graphic designer,web developer,
+                         Research Writer/ Tutor with three years of experience hence you can rest assured that I will guarantee you quality, 
                          top-notch and tailored work.
                          
                      </p> 
@@ -39,13 +59,11 @@
           <h1 class=" text-white text-2xl">Nuxt.js</h1> 
         <div class=" flex  flex-col md:flex-row justify-center items-center m-5 "> 
         
-        <div class=" transition-all duration-500 transform hover:opacity-90 pt-1 hover:pb-1">
+        <div class=" transition-all duration-500 transform hover:opacity-25 pt-1 hover:bg-white">
           <img src="../assets/nuxt.png" alt="" class="  w-56 md:mr-80">
         </div>
         
-          <p class=" ">Tailwind CSS works by scanning all of your HTML files, JavaScript components, and any other templates for class names, generating the corresponding styles and then writing them to a static CSS file.
-  
-  It's fast, flexible, and reliable — with zero-runtime.  </p>
+          <p class=" ">Nuxt provides both frontend and backend functionality so you can focus on what matters: Creating your web application.  </p>
         
         </div>
         </div>
@@ -53,13 +71,15 @@
           <h1 class=" text-white text-2xl">Django</h1> 
         <div class=" flex  flex-col md:flex-row justify-center items-center m-5 "> 
         
-        <div class=" transition-all duration-500 transform hover:opacity-90 pt-1 hover:pb-1">
+        <div class=" transition-all duration-500 transform hover:opacity-90 pt-1 hover:bg-white">
           <img src="../assets/django.png" alt="" class="  w-56 md:mr-80">
         </div>
         
-          <p class=" ">Tailwind CSS works by scanning all of your HTML files, JavaScript components, and any other templates for class names, generating the corresponding styles and then writing them to a static CSS file.
-  
-                                It's fast, flexible, and reliable — with zero-runtime.  </p>
+          <p class=" ">Django is a back-end server side web framework.
+
+Django is free, open source and written in Python.
+
+Django makes it easier to build web pages using Python. </p>
         
         </div>
         </div>
@@ -68,7 +88,7 @@
           <h1 class=" text-white text-2xl">The vue</h1> 
   
           <div class=" flex  flex-col md:flex-row justify-center items-center m-5 ">
-        <div class=" transition-all duration-500 transform hover:opacity-90 pt-1 hover:pb-1">
+        <div class=" transition-all duration-500 transform hover:opacity-90 pt-1 hover:bg-white">
           <img src="../assets/vuejs.png" alt="" class=" w-60 md:mr-80">
         </div>
           <p class=" ">Vue.js is a popular JavaScript framework for front-end development developed and constantly updated by Evan You and his team. You can use this open-source framework to develop any single-page application or user interface. The design is very flexible and focuses on declarative rendering and component composition. </p>
@@ -80,7 +100,7 @@
         <div class=" flex  flex-col md:flex-row justify-center items-center m-5 ">  
           
         
-        <div class=" transition-all duration-500 transform hover:opacity-90 pt-1 hover:pb-1">
+        <div class=" transition-all duration-500 transform hover:opacity-90 pt-1 hover:bg-white">
           <img src="../assets/nodejs.png" alt="" class="  w-56 md:mr-80">
         </div>
           <p class=" ">Node.js is an open source, cross-platform runtime environment for developing server-side and networking applications. Node.js applications are written in JavaScript, and can be run within the Node.js runtime on OS X, Microsoft Windows, and Linux.
@@ -93,7 +113,7 @@
           <h1 class=" text-white text-2xl">Tailwind</h1> 
         <div class=" flex  flex-col md:flex-row justify-center items-center m-5 "> 
         
-        <div class=" transition-all duration-500 transform hover:opacity-90 pt-1 hover:pb-1">
+        <div class=" transition-all duration-500 transform hover:opacity-90 pt-1 hover:bg-white">
           <img src="../assets/tailwindcss.png" alt="" class="  w-56 md:mr-80">
         </div>
         
@@ -103,11 +123,25 @@
         
         </div>
         </div>
+
+        <div class=" pt-3   mt-3 hover:scale-90">
+          <h1 class=" text-white text-2xl">MongoDB</h1> 
+        <div class=" flex  flex-col md:flex-row justify-center items-center m-5 "> 
+        
+        <div class=" transition-all duration-500 transform hover:opacity-90 pt-1 hover:bg-white">
+          <img src="../assets/mongoDB.png" alt="" class="  w-56 md:mr-80">
+        </div>
+        
+          <p class=" ">MongoDB is a document database used to build highly available and scalable internet applications. With its flexible schema approach, it's popular with development teams using agile methodologies..  </p>
+        
+        </div>
+        </div>
+
         <div class=" pt-3   mt-3 hover:scale-90">
           <h1 class=" text-white text-2xl">Bootstrap</h1> 
         <div class=" flex  flex-col md:flex-row justify-center items-center m-5 "> 
         
-        <div class=" transition-all duration-500 transform hover:opacity-90 pt-1 hover:pb-1">
+        <div class=" transition-all duration-500 transform hover:opacity-90 pt-1 hover:bg-white">
           <img src="../assets/bootstrap.png" alt="" class="  w-56 md:mr-80">
         </div>
         
@@ -124,23 +158,33 @@
         
         
        
-  </div>
-  <span class=" flex flex-row items-center justify-center gap-5   mt-4 pt-3 ml-72 mr-52 pb-10">Follow me on 
+
+  <span class=" flex flex-row items-center justify-center gap-5   mt-4 pt-3 ml-72 mr-52 bg-white0">Follow me on 
                                     <a href="https://github.com/Cosmasrono?tab=repositories"><img src="../assets/github.png"></a>
-                                    <a href="https://web.whatsapp.com/"><img src="../assets/w.png"></a>
                                     <a href="https://www.linkedin.com/in/cosmas-cheruiyot-a95a3122b/"><img src="../assets/l.png"></a>
                                    
                              </span>
-  </main>
+  
   </template>
   
-  <script>
-  
-    
-  export default {
+  <script> 
+import Swiper from 'swiper';
 
-    components: {
-    
-    }
-  }
+export default {
+  mounted() {
+    this.initSwiper();
+  },
+  methods: {
+    initSwiper() {
+      const swiper = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+      });
+    },
+  },
+};
+
   </script>
